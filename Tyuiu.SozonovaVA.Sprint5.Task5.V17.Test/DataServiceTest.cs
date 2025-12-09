a80838f359c8931fb883f0b530ab97a7d62c3fa2
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-12-10 04:25:30 +0500

    Task5

--- Tyuiu.SozonovaVA.Sprint5.Task5.V17.Test/DataServiceTest.cs
+++ Tyuiu.SozonovaVA.Sprint5.Task5.V17.Test/DataServiceTest.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.SozonovaVA.Sprint5.Task5.V17.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = $@"C:\Users\Varvara\source\repos\Tyuiu.SozonovaVA.Sprint5\Tyuiu.SozonovaVA.Sprint5.Task5.V17\bin\Debug\InPutDataFileTask5V17.txt";
+            string path = $@"C:\Users\Varvara\source\repos\Tyuiu.SozonovaVA.Sprint5\Tyuiu.SozonovaVA.Sprint5.Task5.V17\bin\Debug\net8.0\InPutDataFileTask5V17.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
