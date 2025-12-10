a825941a0665fb4fae84422010b85237bc18d656
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-12-10 05:43:44 +0500

    Task3

--- Tyuiu.SozonovaVA.Sprint5.Task3.V26.Test/DataServiceTest.cs
+++ Tyuiu.SozonovaVA.Sprint5.Task3.V26.Test/DataServiceTest.cs
@@ -7,7 +7,8 @@ namespace Tyuiu.SozonovaVA.Sprint5.Task3.V26.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\Varvara\source\repos\Tyuiu.SozovovaVA.Sprint5\Tyuiu.SozonovaVA.Sprint5.Task3.V26\bin\Debug\net8.0\OutPutFileTask3.txt";
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask3.txt" });
+            
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
